6d7b7189c0b328eb2eef7dc3105bbbaa4431ecb7
keith ferney <keith@ferney.org>
2022-02-05 17:36:59 -0700

    add player

--- app/player.gd
+++ app/player.gd
@@ -5,27 +5,38 @@ extends RigidBody2D
 # var a = 2
 # var b = "text"
 var charge = 0
-var limit = 120
+var limit = 260
+var limit_divisor = limit/5
 var multiplier = 2
 var rotation_dirs = ["left", "right"]
 var rotation_dir = rotation_dirs[1]
+onready var chargeSprite = $Charge
+onready var pointer = $Pointer
+var frame = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	
 	pass # Replace with function body.
 
 func _physics_process(delta):
+
 	if Input.is_action_just_pressed("ui_down"):
 		if rotation_dir == rotation_dirs[1]:
 			rotation_dir = rotation_dirs[0]
 		else:
 			rotation_dir = rotation_dirs[1] 
-	if Input.is_action_pressed("ui_down") and charge < limit:
-		charge = charge + multiplier
+	if Input.is_action_pressed("ui_down"):
+		if charge < limit:
+			charge = charge + multiplier 
+		chargeSprite.set_frame(int(charge / limit_divisor))
+		pointer.rotate(0.1)
 	elif Input.is_action_just_released("ui_down"):
-		
-		self.apply_impulse(Vector2(0, 0), Vector2(charge, -charge))
-		self.apply_torque_impulse(charge*50)
+		chargeSprite.set_frame(0)
+		self.apply_impulse(Vector2(0, 0), Vector2(0, -charge*2).rotated(pointer.get_global_rotation()))
+		var tqe = 1000
+		if rotation_dir == "left":
+			tqe = -1000
+		self.apply_torque_impulse(tqe)
 		print_debug(charge)
 		charge = 0
 # Called every frame. 'delta' is the elapsed time since the previous frame.
