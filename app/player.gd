7735c44501b0831697937c0d448b97bfefffdf5a
xavierbarbary <kyle@ferney.org>
2022-02-05 18:45:31 -0700

    level

--- app/player.gd
+++ app/player.gd
@@ -67,6 +67,10 @@ func _physics_process(delta):
 		charge = 0
 		
 	chargeSprite.set_frame(frame)
+	
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
